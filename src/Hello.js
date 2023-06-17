c468f59e0531b326341d5cae4869497f21a8d9f5
D1N2O3B4 <davebostons2017@gmail.com>
2023-06-17 18:45:14 +0000

    modified Hello.js

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+class Hello extends Component{
+
+    render(){
+        return(
+            <div>Hey is what you said to me last 2 weeks Friday</div>          
+        )
+    }
+} 
+export default Hello;
